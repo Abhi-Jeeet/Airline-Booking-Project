df0cf1aa80853552df84d5dbb1dfef597b525abc
Abhi-Jeeet <abhijeetkumar3245@gmail.com>
2025-03-30 03:20:03 +0530

    Implemented a functionality to insert a data into a database

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,35 @@
+const{StatusCodes} = require("http-status-codes")
+const {AirplaneService} = require("../services");
+const { response } = require("express");
+
+
+/**
+ * POST : /airplane
+ * req-body {modelNumber: 'Boeing777', capacity:400}
+ */
+
+async function createAirplane(req,res){
+    try {
+        const airplane = await AirplaneService.createAirplane({
+            modelNumber: req.body.modelNumber,
+            capacity: req.body.capacity
+        });
+        return res.status(StatusCodes.CREATED).json({
+            success:true,
+            message: "Successfully create an airplane",
+            data: airplane,
+            error:{}
+        })
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success:false,
+            message:"someting went wrong while creating an airplane",
+            data:{},
+            error:error
+        });
+    }
+}
+
+module.exports={
+    createAirplane
+}
