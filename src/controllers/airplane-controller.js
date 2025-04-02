efcb1328466b48bd0accc914231a6fcd1ee98b8a
Abhi-Jeeet <abhijeetkumar3245@gmail.com>
2025-04-02 23:24:59 +0530

    Implemented success and error response

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,5 +1,6 @@
 const{StatusCodes} = require("http-status-codes")
 const {AirplaneService} = require("../services");
+const {ErrorResponse, SuccessResponse} = require("../utils/common")
 
 
 /**
@@ -13,19 +14,13 @@ async function createAirplane(req,res){
             modelNumber: req.body.modelNumber,
             capacity: req.body.capacity
         });
-        return res.status(StatusCodes.CREATED).json({
-            success:true,
-            message: "Successfully create an airplane",
-            data: airplane,
-            error:{}
-        })
+        SuccessResponse.message="Successfully created an airplane",
+        SuccessResponse.data=airplane
+        return res.status(StatusCodes.CREATED).json(SuccessResponse)
     } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            success:false,
-            message:"someting went wrong while creating an airplane",
-            data:{},
-            error:error
-        });
+        ErrorResponse.message = "someting went wrong while creating an airplane",
+        ErrorResponse.error = error
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
     }
 }
 
