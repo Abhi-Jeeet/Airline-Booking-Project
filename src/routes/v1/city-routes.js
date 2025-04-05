a1478d960e798f8013b36335ef401ac0d4b4d990
Abhi-Jeeet <abhijeetkumar3245@gmail.com>
2025-04-05 22:55:55 +0530

    Implemented getcities, getcity, destroyCity, updateCity api

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -7,5 +7,17 @@ const router = express.Router();
 // /api/v1/cities POST
 router.post('/', CityMiddlewares.validateRequest ,CityController.createCity);
 
+// /api/v1/cities GET
+router.get('/', CityController.getCities);
+
+// /api/v1/cities/:id GET
+router.get('/:id', CityController.getCity);
+
+// /api/v1/cities/:id DELETE
+router.delete('/:id', CityController.destroyCity);
+
+// /api/v1/cities/:id Patch
+router.patch("/:id", CityController.updateCity);
+
 
 module.exports = router
